6c66eed7fecfbb84e457e39ee3a3ff1376692eda
Chrysanthemum Lovelace <chryslovelace@gmail.com>
2025-02-19 09:12:57 -0500

    jwt public key authentication

--- Microservice.cs
+++ Microservice.cs
@@ -1,5 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
 using CommandLine;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Cryptography;
 
 static class Microservice
 {
@@ -12,6 +15,25 @@ static class Microservice
         builder.Services.AddSingleton(database);
         builder.Services.AddSingleton(transkribusClient);
         builder.Services.AddScoped<Processor>();
+        if (bool.TryParse(configuration["USE_JWT_AUTHENTICATION"], out var useJwt) && useJwt) 
+        {
+            if (string.IsNullOrEmpty(configuration["JWT_PUBLIC_KEY"])) 
+            {
+                throw new InvalidOperationException("JWT_PUBLIC_KEY is required for JWT Authentication.");
+            }
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(jwtOptions => 
+            {
+                var rsa = RSA.Create();
+                rsa.ImportRSAPublicKey(Convert.FromBase64String(configuration["JWT_PUBLIC_KEY"]), out var _);
+                jwtOptions.TokenValidationParameters = new TokenValidationParameters 
+                {
+                    IssuerSigningKey = new RsaSecurityKey(rsa),
+                    ValidateIssuer = false,
+                    RequireAudience = false,
+                    ValidateAudience = false
+                };
+            });
+        }
         var app = builder.Build();
         app.MapGet("/", (
             [FromHeader(Name = "X-Islandora-Args")] string args,
